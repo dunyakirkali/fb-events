9f428078218e5f208d050750aecd837bb22fa5ba
Dunya Kirkali <dunyakirkali@gmail.com>
2016-04-02 22:32:32 +0200

    Prepare facebook

--- facebook-events.php
+++ facebook-events.php
@@ -9,11 +9,16 @@
   * License: GPL2
   */
 
+  session_start();
+  include('src/facebook.php');
+
   add_action('admin_menu', 'fb_events_menu');
   add_action('admin_init', 'fb_events_settings');
   add_shortcode('fb_events', 'fb_events_list');
 
+
   function fb_events_settings() {
+    register_setting('fb-events-settings-group', 'fb_page_id');
 	  register_setting('fb-events-settings-group', 'fb_page_token');
   }
 
@@ -33,6 +38,10 @@
                 <th scope="row"><label for="fb_page_token">FB Page token</label></th>
                 <td><input name="fb_page_token" type="text" value="<?php echo esc_attr( get_option('fb_page_token') ); ?>" class="regular-text"></td>
               </tr>
+              <tr>
+                <th scope="row"><label for="fb_page_id">FB Page id</label></th>
+                <td><input name="fb_page_id" type="text" value="<?php echo esc_attr( get_option('fb_page_id') ); ?>" class="regular-text"></td>
+              </tr>
             </table>
             <?php submit_button(); ?>
         </form>
@@ -41,11 +50,37 @@
   }
 
   function fb_events_list($atts) {
+    $url = get_option('fb_page_id') . "/events";
+    $events = [['name' => 'Dunya'], ['name' => 'Ilana']];
+    echo $session;
+    echo $url;
+    // $fb = new Facebook\Facebook([
+    //   'app_id' => '{app-id}',
+    //   'app_secret' => '{app-secret}',
+    //   'default_graph_version' => 'v2.5',
+    //   'default_access_token' => get_option('fb_page_token')
+    // ]);
+    try {
+      // Get the Facebook\GraphNodes\GraphUser object for the current user.
+      // If you provided a 'default_access_token', the '{access-token}' is optional.
+      // $response = $fb->get('/me', '{access-token}');
+    } catch(Facebook\Exceptions\FacebookResponseException $e) {
+      // When Graph returns an error
+      echo 'Graph returned an error: ' . $e->getMessage();
+      exit;
+    } catch(Facebook\Exceptions\FacebookSDKException $e) {
+      // When validation fails or other local issues
+      echo 'Facebook SDK returned an error: ' . $e->getMessage();
+      exit;
+    }
     ?>
-      <ul>
-        <li>A</li>
-        <li>B</li>
-        <li>C</li>
+
+    <ul>
+      <?php
+        for ($x = 0; $x < count($events); $x++) {
+          echo "<li>$x</li>";
+        }
+      ?>
       </ul>
     <?php
   }
